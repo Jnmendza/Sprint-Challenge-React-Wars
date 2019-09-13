a62dfc1556e4aff9d55c1369746fba7586de22ea
Jnmendza <jonatmendoza@gmail.com>
2019-09-13 09:24:55 -0700

    Components are showing up on brower

--- starwars/src/components/PeopleCard.js
+++ starwars/src/components/PeopleCard.js
@@ -1,1 +1,21 @@
 import React from "react";
+import styled from "styled-components";
+
+const Cards = styled.div`
+    background: pink;
+
+`;
+
+const PeopleCard = props => {
+    return (
+        <Cards>
+            <h1>{props.name}</h1>
+            <p>Weight: {props.mass}</p>
+            <p>Height:{props.height}</p>
+            <p>Was born in: {props.birth_year}</p>
+
+        </Cards>
+    );
+};
+
+export default PeopleCard;
--- starwars/src/components/PeopleGrid.js
+++ starwars/src/components/PeopleGrid.js
@@ -1,3 +1,30 @@
 import React, { useState } from "react";
 import { people } from "../data";
 import PeopleCard from "./PeopleCard";
+import styled from "styled-components";
+
+const CardContainer = styled.div`
+    display: flex;
+`;
+
+const PeopleGrid = () => {
+    const [peopleState] = useState(people);
+    console.log(peopleState)
+
+    return (
+        <div className="card-container">
+            {peopleState.map((key) =>(
+                <PeopleCard 
+                key={key}
+                name={key.name}
+                mass={key.mass}
+                height={key.height}
+                birth_year={key.birth_year}
+                />
+            ))}    
+
+        </div>
+    )
+};
+
+export default PeopleGrid;
